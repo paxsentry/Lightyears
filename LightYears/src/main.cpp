006327fc8ff20bc1aaa63d2d27d6210ce68581d5
Zoltan Stomfai <zoltan.stomfai@gmail.com>
2025-05-14 17:10:26 +0200

    Setup post build actions

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
+    sf::RenderWindow renderWindow{ sf::VideoMode(sf::Vector2u(100,100)), "My Window" };
     std::cout << "Hello world!\n";
 }
