68b3073c016fccb4e23e4812f6b1d324b59c3765
Zoltan Stomfai <zoltan.stomfai@gmail.com>
2025-05-15 14:10:01 +0200

    FPS set logic added

--- LightYears/include/framework/Application.h
+++ LightYears/include/framework/Application.h
@@ -7,8 +7,17 @@ namespace ly
     {
     public:
         Application();
+
         void Run();
+
     private:
+        void Tick(float deltaTime);
+        void Render();
+
         sf::RenderWindow mWindow;
+
+        float mTargetFrameRate;
+
+        sf::Clock mTickClock;
     };
 }
--- LightYears/src/framework/Application.cpp
+++ LightYears/src/framework/Application.cpp
@@ -1,13 +1,21 @@
 #include "framework/Application.h"
+#include <iostream>
 
 namespace ly
 {
     Application::Application()
-        :mWindow{ sf::VideoMode(sf::Vector2u(1024,1440)), "Light Years" }
-    { }
+        :mWindow{ sf::VideoMode(sf::Vector2u(1024,1440)), "Light Years" },
+        mTargetFrameRate{ 60.f },
+        mTickClock{ }
+    {
+    }
 
     void Application::Run()
     {
+        mTickClock.restart();
+        float accumulatedTime = 0.f;
+        float targetDeltaTime = 1.f / mTargetFrameRate;
+
         while (mWindow.isOpen())
         {
             while (const std::optional windowEvent = mWindow.pollEvent())
@@ -16,6 +24,28 @@ namespace ly
                     mWindow.close();
                 }
             }
+
+            float frameDeltaTime = mTickClock.restart().asSeconds();
+            accumulatedTime += frameDeltaTime;
+            while (accumulatedTime > targetDeltaTime)
+            {
+                accumulatedTime -= targetDeltaTime;
+                Tick(targetDeltaTime);
+                Render();
+            }
+
+            // The actual framerate
+            //std::cout << "ticking at FPS: " << 1.f / frameDeltaTime << std::endl;
         }
+    }
+
+    void Application::Tick(float deltaTime)
+    {
+        std::cout << "ticking at the set FPS: " << 1.f / deltaTime << std::endl;
+    }
+
+    void Application::Render()
+    {
+
     }
 }
